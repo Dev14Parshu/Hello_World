ac85a7d75614ad18eb6f03e52fe7deda51b76d14
Dev14Parshu <developerparshuram@gmail.com>
2024-02-29 19:27:36 +0530

    Initial Commit

---
+++ src/com/parsh/dev/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.parsh.dev;
+
+/**
+ * @author parsh
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+
+	}
+
+}
